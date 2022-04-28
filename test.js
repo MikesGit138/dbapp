6d3ae54060aa51c702c7fdfee668a2405babc3a1
MikesGit138 <michael138leighton@gmail.com>
2022-04-27 20:12:38 -0500

    using express

--- test.js
+++ test.js
@@ -1,13 +1,40 @@
-var mysql = require('mysql')
+const mysql = require('mysql')
+const express = require('express')
 
-var conn = mysql.createConnection({
+
+//create connection
+const db = mysql.createConnection({
     host : 'localhost',
     user : 'root',
     password : 'root',
     database : 'amberapp1'
 })
 
-conn.query ('SELECT * FROM amberapp1.students', function (err, results, fields){
-    if (err) throw err;
-    console.table(results)
+//connect
+db.connect((err)=> {
+    if (err){
+        throw err;
+    } else {
+        console.log('mysql connected...')
+    }
 })
+
+const app = express();
+
+app.get('/students' , (req, res) => {
+    conn.query('SELECT * FROM amberapp1.students', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+        })
+    }); 
+
+app.listen('3000', () => {
+    console.log('server started on porter 3000')
+})
+
+// db.query ('SELECT * FROM amberapp1.students', function (err, results, fields){
+//     if (err) throw err;
+//     console.table(results)
+// })
